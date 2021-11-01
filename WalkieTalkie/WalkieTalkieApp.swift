c02bd7b1dde48cdb6e66441a0f79d33fe0c23d62
Brett Keck <brett@barrylliumlabs.com>
2021-11-01 11:59:27 -0400

    Initial Commit

---
+++ WalkieTalkie/WalkieTalkieApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkieTalkieApp.swift
+//  WalkieTalkie
+//
+//  Created by Brett Keck on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct WalkieTalkieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
