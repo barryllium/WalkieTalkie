52931d7ad9c4c34f96c74955934d6114addd0955
Brett Keck <brett@barrylliumlabs.com>
2021-11-02 14:29:45 -0400

    App Icon

--- WalkieTalkie/Extensions/Color+Extension.swift
+++ WalkieTalkie/Extensions/Color+Extension.swift
@@ -13,5 +13,4 @@ extension Color {
     static let lightHighlightColor = Color(red: 228.0/255.0, green: 228.0/255.0, blue: 231.0/255.0)
     static let darkHighlightColor = Color(red: 185.0/255.0, green: 193.0/255.0, blue: 205.0/255.0)
     static let appBlue = Color(red: 33.0/255.0, green: 115.0/255.0, blue: 184.0/255.0)
-    static let appYellow = Color(red: 258.0/255.0, green: 218.0/255.0, blue: 54.0/255.0)
 }
